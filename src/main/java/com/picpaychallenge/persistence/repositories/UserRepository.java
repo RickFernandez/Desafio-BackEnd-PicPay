292c5c395cbd92976b9bb8ada441fef73cc00663
Henrique Fernandez <rickfernandez03@gmail.com>
2024-01-15 21:52:26 -0300

    refactor: refactor UserRepository

--- src/main/java/com/picpaychallenge/persistence/repositories/UserRepository.java
+++ src/main/java/com/picpaychallenge/persistence/repositories/UserRepository.java
@@ -1,10 +1,11 @@
 package com.picpaychallenge.persistence.repositories;
 
+import com.picpaychallenge.persistence.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserRepository, Long> {
-    Optional<UserRepository> findUserByDocument(String document);
-    Optional<UserRepository> findUserById(Long id);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
 }
