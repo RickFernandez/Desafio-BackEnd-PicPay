70a6b40407aab7053d9faf29e5fa34586d64ca51
Henrique Fernandez <rickfernandez03@gmail.com>
2024-01-17 14:25:57 -0300

    feat: add validation in Notification API data

--- src/main/java/com/picpaychallenge/dtos/requests/NotificationRequest.java
+++ src/main/java/com/picpaychallenge/dtos/requests/NotificationRequest.java
@@ -1,4 +1,12 @@
 package com.picpaychallenge.dtos.requests;
 
-public record NotificationRequest(String email, String message) {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record NotificationRequest(
+        @NotBlank(message = "O campo email é obrigatório")
+        @Email(message = "Email inválido")
+        String email,
+        @NotBlank(message = "O campo mensagem é obrigatório")
+        String message
+) { }
